1e5af0abd548b65a28aa503e21ffc47bee18612e
Jackson Nace <jacksonnace964@gmail.com>
2025-01-18 23:16:27 -0600

    fixed .env import

--- tobybot.py
+++ tobybot.py
@@ -3,6 +3,7 @@ import discord
 from dotenv import load_dotenv
 import os
 load_dotenv()
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
 class Client(discord.Client):
   async def on_ready(self): #onready function is a built in discord function that runs when connected to discord
@@ -15,5 +16,4 @@ intents = discord.Intents.default() # using default permissions
 intents.message_content = True
 
 client = Client (intents=intents)
-token = os.environ(DISCORD_BOT_TOKEN)
-client.run(token)
+client.run(DISCORD_BOT_TOKEN)
